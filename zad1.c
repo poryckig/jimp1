acdf2763e4d4bb173564c486c0e3ae46afb2552a
Grzegorz Porycki <poryckig@gmail.com>
2021-12-13 23:30:39 +0100

    zmodyfikowanie2 zad1.c

--- zad1.c
+++ zad1.c
@@ -49,12 +49,8 @@ ur_t czytaj( char *nazwa_pliku ) {
     return u;
 }
 
-double *solve( ur_t ur ) {
+void eliminacja_gaussa(ur_t ur){
     int n = ur->n;
-    if( n <= 0 )
-        return NULL;
-    double *x = malloc( n * sizeof *x );
-    // Przekształcam u tak, aby A stała się macierzą górną trójkątną    //***METODA ELIMINACJI GAUSSA(macierz gorna trojka)
     double *A = ur->a;
     double *b = ur->b;
     for( int k= 0; k < n-1; k++ ) {
@@ -64,13 +60,49 @@ double *solve( ur_t ur ) {
                 A[w*n+i] -= q * A[k*n+i];
             b[w] -= q*b[k] ;
         }
-    }                                        //***PODSTAWIENIE WSTECZNE***
+    }                           
+}
+void podstawienie_wsteczne(ur_t ur, double *x){
+    int n = ur->n;
+    double *A = ur->a;
+    double *b = ur->b;
     for( int w= n-1; w >= 0; w-- ) {
         double s = 0;
         for( int k= w+1; k < n; k++ )
             s += A[w*n+k]*x[k];
         x[w] = ( b[w] - s ) / A[w*n+w];
     }
+}
+
+double *solve( ur_t ur ) {
+    int n = ur->n;
+    if( n <= 0 )
+        return NULL;
+    double *x = malloc( n * sizeof *x );
+    double *A = ur->a;
+    double *b = ur->b;
+    eliminacja_gaussa(ur);
+    // Przekształcam u tak, aby A stała się macierzą górną trójkątną    //***METODA ELIMINACJI GAUSSA(macierz gorna trojka)
+    /*
+    double *A = ur->a;
+    double *b = ur->b;
+    for( int k= 0; k < n-1; k++ ) {
+        for( int w= k+1; w < n; w++ ) {
+            double q = A[w*n+k] / A[k*n+k];     // A[i*n+j] to A(i,j)
+            for( int i= k; i < n; i++ )
+                A[w*n+i] -= q * A[k*n+i];
+            b[w] -= q*b[k] ;
+        }
+    } */
+    podstawienie_wsteczne(ur, x);
+/*                                          ****PODSTAWIENIE WSTECZNE***
+    for( int w= n-1; w >= 0; w-- ) {
+        double s = 0;
+        for( int k= w+1; k < n; k++ )
+            s += A[w*n+k]*x[k];
+        x[w] = ( b[w] - s ) / A[w*n+w];
+    }
+*/
     return x;
 }
 
