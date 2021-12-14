2d80d9036ecc39404eb1b5712d980cfbe9a05e37
Michał Kwiatkowski <01169586@pw.edu.pl>
2021-12-14 22:33:54 +0100

    modyfikacja pliku backsubst.c

--- program/backsubst.c
+++ program/backsubst.c
@@ -5,18 +5,21 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	int w;
+	int n = mat->r;
+	if (b->r != n) return 2;
+	if (mat->c != n) return 2;
+	for (w = n-1; w >= 0; w-- ){
+		if(mat->data[w][w] == 0) return 1;
+		double s = 0;
+		for (int k = w+1; k < n; k++){
+			s += mat->data[w][k] * x->data[k][0];
+		}
+		x->data[w][0] = ( b->data[w][0] - s) / mat->data[w][w];
+	}
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+	return 0;
 }
 
 
